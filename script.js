716ef948ec1bdf5f0603f5a88623d3e2286e5436
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-06 11:14:17 -0400

    adding pokemon dynamically and navigating the API

--- script.js
+++ script.js
@@ -24,52 +24,56 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
 
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
+        let selectedPokemon = e.target["pokemon-select"].value;
+        // console.log("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
+        let errMessage = document.querySelector("#error-message");
+        if( selectedPokemon !== "default" ){
+            console.log(selectedPokemon);
+            errMessage.textContent = ""; // erases the error message
+            fetch("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
+                .then((res)=>res.json())
+                .then((data)=>{
+                    // console.log(data);
+                    let details = document.querySelector("#details");
 
-        let details = document.querySelector("#details");
-        let url = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png";
-        /* creating a template literal for the URL to be dynamic */
-        details.innerHTML = `
-        <div id="details-title">
-            <h2>Details</h2>
-        </div>
-        <div id="details-img-container">
-            <img src="${url}" alt="Image of selected Pokémon" />
-        </div>
-        <div id="details-text">
-            <div id="details-name">Name: Squirtle</div>
-            <div id="details-species">Species: Squirtle</div>
-            <div id="details-type">Type: Electric</div>
-        </div>
-        <div id="details-sub-text">
-            <h3>Base Attributes</h3>
-            <div>Hit Points: 35</div>
-            <div>Attack: 55</div>
-            <div>Defense: 40</div>  
-        </div>
-        <div id="details-action">
-            <!-- button that's not in a form -->
-            <button>Add -></button>
-        </div>`
+                    // let typeStr = data.types.map((typeEl)=>typeEl.type.name).join("/");
+                    let typeStr = data.types.map((typeEl)=>{
+                        return typeEl.type.name;
+                    }).join("/");
 
-        // let selectedPokemon = e.target["pokemon-select"].value;
+                    /* creating a template literal using interpolation for the URL to be dynamic */
+                    details.innerHTML = `
+                    <div id="details-title">
+                        <h2>Details</h2>
+                    </div>
+                    <div id="details-img-container">
+                        <img src="${data.sprites.front_default}" alt="Image of selected Pokémon" />
+                    </div>
+                    <div id="details-text">
+                        <div id="details-name">Name: ${data.name}</div>
+                        <div id="details-species">Species: ${typeStr}</div>
+                        <div id="details-type">Type: Electric</div>
+                    </div>
+                    <div id="details-sub-text">
+                        <h3>Base Attributes</h3>
+                        <div>Hit Points: 35</div>
+                        <div>Attack: 55</div>
+                        <div>Defense: 40</div>  
+                    </div>
+                    <div id="details-action">
+                        <!-- button that's not in a form -->
+                        <button>Add -></button>
+                    </div>`
+                    // let currentPokemonEl = document.querySelector("#current-pokemon");
 
-//         // console.log("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
-//         let errMessage = document.querySelector("#error-message");
-//         if( selectedPokemon !== "default" ){
-//             errMessage.textContent = "";
-//             fetch("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
-//                 .then((res)=>res.json())
-//                 .then((data)=>{
-//                     let currentPokemonEl = document.querySelector("#current-pokemon");
-
-//                     let img = document.createElement("img");
-//                     img.src = data.sprites["front_default"];
+                    // let img = document.createElement("img");
+                    // img.src = data.sprites["front_default"];
                     
-//                     currentPokemonEl.append(img);
-//                 });
-//         } else {
-//             errMessage.textContent = "Please select a Pokemon!";
-//         }
+                    // currentPokemonEl.append(img);
+                });
+        } else {
+            errMessage.textContent = "Please choose a Pokémon!";
+        }
 
-//         // console.log(selectedPokemon);
+        // console.log(selectedPokemon);
     });
