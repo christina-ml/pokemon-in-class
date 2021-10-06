f8005d01bebc4bf53202c7d8c9ef7a27d934f204
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-06 11:42:21 -0400

    use accumulator to get hp, atk, def. Add to string interpolation when pokemon is chosen

--- script.js
+++ script.js
@@ -33,9 +33,28 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
             fetch("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
                 .then((res)=>res.json())
                 .then((data)=>{
-                    // console.log(data);
+                    /* use accumulator pattern */
+                    let hp; // HP
+                    let atk; // Atk
+                    let def;  // Def
+
+                    for (let statObj of data.stats){
+                        if (statObj.stat.name === "hp"){
+                            hp = statObj.base_stat;
+                        } else if (statObj.stat.name === "attack"){
+                            atk = statObj.base_stat;
+                        } else if (statObj.stat.name === "defense"){
+                            def = statObj.base_stat;
+                        }
+                    }
+
+                    console.log(hp); // HP
+                    console.log(atk); // Atk
+                    console.log(def); // Def
+
                     let details = document.querySelector("#details");
 
+                    /* One-liner vs. writing it out */
                     // let typeStr = data.types.map((typeEl)=>typeEl.type.name).join("/");
                     let typeStr = data.types.map((typeEl)=>{
                         return typeEl.type.name;
@@ -52,13 +71,14 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
                     <div id="details-text">
                         <div id="details-name">Name: ${data.name}</div>
                         <div id="details-species">Species: ${typeStr}</div>
-                        <div id="details-type">Type: Electric</div>
+                        <div id="details-weight">Weight: ${data.weight}lbs.</div>
+                        <div id="details-height">Height: ${data.height}ft.</div>
                     </div>
                     <div id="details-sub-text">
                         <h3>Base Attributes</h3>
-                        <div>Hit Points: 35</div>
-                        <div>Attack: 55</div>
-                        <div>Defense: 40</div>  
+                        <div>Hit Points: ${hp}</div>
+                        <div>Attack: ${atk}</div>
+                        <div>Defense: ${def}</div>  
                     </div>
                     <div id="details-action">
                         <!-- button that's not in a form -->
