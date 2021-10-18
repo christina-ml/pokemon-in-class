262d46edf64e9f21f8b0472ba95a31ce75769fb7
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-18 11:29:49 -0400

    accepted incoming changes from greg's code

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-/* Made code dynamic by fetching based on what pokemon name you put in function */
-function fetchPokemonDetails(pokemonName, shouldAddToRecent) {
-
-    let errMessage = document.querySelector("#error-message");
-    if( pokemonName !== "default" ){
-        // console.log(selectedPokemon);
-        errMessage.textContent = ""; // erases the error message
-        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
-            .then((res)=>res.json())
-            .then((data)=>{
-                /* use accumulator pattern */
-                let hp; // HP
-                let atk; // Atk
-                let def;  // Def
-
-                for (let statObj of data.stats){
-                    if (statObj.stat.name === "hp"){
-                        hp = statObj.base_stat;
-                    } else if (statObj.stat.name === "attack"){
-                        atk = statObj.base_stat;
-                    } else if (statObj.stat.name === "defense"){
-=======
 function fetchPokemonDetails(pokemonName, shouldAddToRecent){
 
     let errMessage = document.querySelector("#error-message");
@@ -40,86 +17,16 @@ function fetchPokemonDetails(pokemonName, shouldAddToRecent){
                     } else if(statObj.stat.name === "attack"){
                         atk = statObj.base_stat;
                     } else if(statObj.stat.name === "defense"){
->>>>>>> a94f02f3f316ddec5902410f0a0ce9222b3fa263
                         def = statObj.base_stat;
                     }
                 }
 
-<<<<<<< HEAD
-                // console.log(hp); // HP
-                // console.log(atk); // Atk
-                // console.log(def); // Def
-
                 let details = document.querySelector("#details");
 
-                /* One-liner vs. writing it out */
-                // let typeStr = data.types.map((typeEl)=>typeEl.type.name).join("/");
-=======
-                let details = document.querySelector("#details");
-
->>>>>>> a94f02f3f316ddec5902410f0a0ce9222b3fa263
                 let typeStr = data.types.map((typeEl)=>{
                     return typeEl.type.name;
                 }).join("/");
 
-<<<<<<< HEAD
-                /* creating a template literal using interpolation for the URL to be dynamic */
-                details.innerHTML = `
-                    <div id="details-title">
-                        <h2>Details</h2>
-                    </div>
-                    <div id="details-img-container">
-                        <img src="${data.sprites.front_default}" alt="Image of selected Pokémon" />
-                    </div>
-                    <div id="details-text">
-                        <div id="details-name">Name: ${data.name}</div>
-                        <div id="details-species">Species: ${typeStr}</div>
-                        <div id="details-weight">Weight: ${data.weight} hectograms</div>
-                        <div id="details-height">Height: ${data.height} decimeters</div>
-                    </div>
-                    <div id="details-sub-text">
-                        <h3>Base Attributes</h3>
-                        <div>Hit Points: ${hp}</div>
-                        <div>Attack: ${atk}</div>
-                        <div>Defense: ${def}</div>  
-                    </div>`
-                    // <div id="details-action">
-                    //     <!-- button that's not in a form -->
-                    //     <button>Add -></button>
-                    // </div>`
-
-
-                /* IF add to recent is true, add this code/add to recent list.
-                If false, skip it completely */
-                if(shouldAddToRecent){
-                    let recentList = document.querySelector("#recent-list");
-                    // <div class="recent-list-items">
-                    //     <img src="https://static.pokemonpets.com/images/monsters-images-800-800/147-Dratini.webp" alt="Evolution version image" />
-                    //     <div>Dratini</div>
-                    // </div>
-    
-                    let recentListItem = document.createElement("div");
-                    recentListItem.classList.add("recent-list-item");
-    
-                    let recentListImg = document.createElement("img");
-                    recentListImg.src = data.sprites.front_default;
-                    recentListImg.alt = "evolution version image";
-                    
-                    let nameDiv = document.createElement("div");
-                    nameDiv.textContent = data.name;
-    
-                    /* Make recent list item NAME (only) clickable */
-                    nameDiv.addEventListener("click", (event)=>{
-                        // console.log(event);
-                        fetchPokemonDetails(event.target.textContent, false); // change details
-                    });
-    
-                    recentListItem.append(recentListImg, nameDiv);
-    
-                    recentList.append(recentListItem);
-                }
-
-=======
                 details.innerHTML = `<div id="details-title">
                         <h2>Details</h2>
                     </div>
@@ -164,7 +71,6 @@ function fetchPokemonDetails(pokemonName, shouldAddToRecent){
     
                         recentList.append(recentListItem);
                     }
->>>>>>> a94f02f3f316ddec5902410f0a0ce9222b3fa263
 
                 // let currentPokemonEl = document.querySelector("#current-pokemon");
 
@@ -173,13 +79,8 @@ function fetchPokemonDetails(pokemonName, shouldAddToRecent){
                 
                 // currentPokemonEl.append(img);
             });
-<<<<<<< HEAD
-        } else {
-        errMessage.textContent = "Please choose a Pokémon!";
-=======
     } else {
         errMessage.textContent = "Please choose a Pokemon!";
->>>>>>> a94f02f3f316ddec5902410f0a0ce9222b3fa263
     }
 }
 
@@ -189,10 +90,6 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
     }).then((data)=>{
         let pokemonList = data.results;
         for(let pokemon of pokemonList){
-<<<<<<< HEAD
-            /* Destructuring - extracted name from object, instead of using `pokemon.name` can just use `name` */
-=======
->>>>>>> a94f02f3f316ddec5902410f0a0ce9222b3fa263
             let { name } = pokemon;
             let select = document.querySelector("#pokemon-selector select");
             
@@ -201,11 +98,6 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
             newOption.textContent = name[0].toUpperCase() + name.slice(1);
             newOption.value = name;
             /////////////////// Option
-<<<<<<< HEAD
-            
-=======
-
->>>>>>> a94f02f3f316ddec5902410f0a0ce9222b3fa263
             select.append(newOption);
         }
     }).catch((err)=>{
@@ -217,29 +109,6 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
         let selectedPokemon = e.target["pokemon-select"].value;
-<<<<<<< HEAD
-        // console.log("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
-       
-            /* REMOVED CODE FROM HERE TO MAKE HELPER FUNCTION (Above)
-                Moved code into another function.
-            */
-            /* Passing in the name of the selected pokemon */
-            fetchPokemonDetails(selectedPokemon, true); // passing in string
-
-        // console.log(selectedPokemon);
-    });
-
-
-/* START - Add to team button */
-let addToTeamButton = document.querySelector("#add-to-team");
-
-addToTeamButton.addEventListener("click", ()=>{
-    let currentPokemonName = document.querySelector("#details-name").textContent;
-    console.log(currentPokemonName);
-    // let currentPokemonImg;
-})
-/* END - Add to team button */
-=======
 //         // console.log("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
         fetchPokemonDetails(selectedPokemon, true);
 
@@ -317,4 +186,3 @@ clearTeamButton.addEventListener("click", ()=>{
         member.remove();
     }
 });
->>>>>>> a94f02f3f316ddec5902410f0a0ce9222b3fa263
